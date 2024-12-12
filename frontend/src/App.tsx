89396db0b665f103a293c2d1b561d0b884075276
Julio Herrera <cesarchavarria0@gmail.com>
2024-12-12 16:40:02 -0600

    Setting up frontend project - React

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Tasks from "./pages/tasks/index.tsx";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Tasks />
+    </div>
+  );
+}
